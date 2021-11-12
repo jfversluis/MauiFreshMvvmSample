5eb314c2fdd92c1a19690dedc7f01f4d8e8f2746
Gerald Versluis <gerald@verslu.is>
2021-11-12 14:03:36 +0100

    Initial commit

---
+++ MauiFreshMvvmSample/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace MauiFreshMvvmSample
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			var tabbedPage = new FreshMvvm.Maui.FreshTabbedNavigationContainer();
+			tabbedPage.AddTab<MainPageModel>("Main", null);
+			tabbedPage.AddTab<MainPageModel>("Main 1", null);
+
+			MainPage = tabbedPage;
+		}
+	}
+}
---
+++ MauiFreshMvvmSample/MainPageModel.cs
@@ -0,0 +1,14 @@
+﻿using FreshMvvm.Maui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiFreshMvvmSample
+{
+    internal class MainPageModel : FreshBasePageModel
+    {
+        public string MyHello { get; set; } = "Hellow!";
+    }
+}
